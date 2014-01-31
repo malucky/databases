4dd8e3ffe77923ce908cdd6128880a4291c9bd47
Hack Reactor Students <rhm262@gmail.com>
2014-01-31 12:19:13 -0800

    Successfully created database.

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,13 +1,115 @@
-CREATE DATABASE chat;
+CREATE DATABASE raybenchat5;
 
-USE chat;
+USE raybenchat5;
 
-CREATE TABLE messages (
- /* Describe your table here.*/
-);
+-- CREATE TABLE messages (
+--  /* Describe your table here.*/
+--  `id` INT NOT NULL AUTO_INCREMENT,
+--  message varchar(255),
+--  id_users TINYINT NULL DEFAULT NULL,
+--  id_rooms TINYINT NULL DEFAULT NULL,
+--  PRIMARY KEY ( id ),
+--  FOREIGN KEY ( id_users ) REFERENCES users(`id`),
+--  FOREIGN KEY ( id_rooms ) REFERENCES rooms(`id`)
+-- );
+
+-- CREATE TABLE users (
+--  /* Describe your table here.*/
+--  id INT NOT NULL AUTO_INCREMENT,
+--  username varchar(255),
+--  PRIMARY KEY ( id )
+-- );
+
+-- CREATE TABLE rooms (
+--  /* Describe your table here.*/
+--  id INT NOT NULL AUTO_INCREMENT,
+--  roomname varchar(255),
+--  PRIMARY KEY ( id )
+-- );
+
 
 /* You can also create more tables, if you need them... */
 
 /*  Execute this file from the command line by typing:
  *    mysql < schema.sql
  *  to create the database and the tables.*/
+
+
+
+
+
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+		
+CREATE TABLE `messages` (
+  `id` TINYINT NULL AUTO_INCREMENT DEFAULT NULL,
+  `text` VARCHAR(255) NULL DEFAULT NULL,
+  `id_users` TINYINT NULL DEFAULT NULL,
+  `time` DATETIME NULL DEFAULT NULL,
+  `id_rooms` TINYINT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+		
+CREATE TABLE `users` (
+  `id` TINYINT NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'rooms'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `rooms`;
+		
+CREATE TABLE `rooms` (
+  `id` TINYINT NULL AUTO_INCREMENT DEFAULT NULL,
+  `roomname` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (id_rooms) REFERENCES `rooms` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`text`,`id_users`,`time`,`id_rooms`) VALUES
+-- ('','','','','');
+-- INSERT INTO `users` (`id`,`username`) VALUES
+-- ('','');
+-- INSERT INTO `rooms` (`id`,`roomname`) VALUES
+-- ('','');
+
